5662e93a8654c9e4b533c0dfab3cbfc1daf6d5f5
Amber Song <jihongsongnz@gmail.com>
2021-11-08 11:51:25 +1300

    Write a simple react app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 /**
  * This is the entry point for webpack to process
 */
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./app.jsx";
+ReactDOM.render(<App />, document.getElementById("root"));
