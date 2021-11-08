2be640d5dfc0be7f4dfd4772226a42e30b0f8113
Amber Song <jihongsongnz@gmail.com>
2021-11-08 13:16:51 +1300

    Remove file from main

--- src/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * This is the entry point for webpack to process
-*/
-
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./app.jsx";
-ReactDOM.render(<App />, document.getElementById("root"));
